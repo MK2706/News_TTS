ec8b00722007bef81483c6b65c529fead343fb07
MK2706 <mn2002manoj@gmail.com>
2025-03-20 14:05:47 +0530

    keyword_extraction.py

--- News_TTs/keyword_extraction.py
+++ News_TTs/keyword_extraction.py
@@ -8,12 +8,13 @@ import yake
 from collections import Counter
 
 def extract_topics(texts):
-    kw_extractor = yake.KeywordExtractor()
+    kw_extractor = yake.KeywordExtractor(n=5, dedupLim=0.9)
     keywords = []
+
     for text in texts:
         extracted_keywords = kw_extractor.extract_keywords(text)
-        keywords.extend([kw[0] for kw in extracted_keywords[:3]])  # Top 3 keywords per text
-    # Get the most common keywords
-    common_keywords = Counter(keywords).most_common(5)  # Top 5 most common keywords
-    return [kw[0] for kw in common_keywords]
+        keywords.extend([kw[0] for kw in extracted_keywords[:5]])  # Top 5 keywords per text
 
+    # Get the most common keywords
+    common_keywords = Counter(keywords).most_common(5)
+    return [kw[0] for kw in common_keywords]
